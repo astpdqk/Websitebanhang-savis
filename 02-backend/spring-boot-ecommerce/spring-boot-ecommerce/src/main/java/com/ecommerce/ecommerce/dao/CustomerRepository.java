d739e529107a6d83001d4d0567d394e776ec4d0c
PhamHongKhoi <khoiphph19840@fpt.edu.vn>
2023-11-18 15:42:20 +0700

    update file

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/ecommerce/ecommerce/dao/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.ecommerce.dao;
-
-import com.ecommerce.ecommerce.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
